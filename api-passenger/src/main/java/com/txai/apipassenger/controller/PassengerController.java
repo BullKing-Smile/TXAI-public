1f880362fe543c4be6d5b5f2f40f7a5ff9abfa25
BullKing-Smile <bull.king.smile@gmail.com>
2025-06-20 20:40:21 +0800

    jwt interceptor

--- api-passenger/src/main/java/com/txai/apipassenger/controller/PassengerController.java
+++ api-passenger/src/main/java/com/txai/apipassenger/controller/PassengerController.java
@@ -1,5 +1,6 @@
 package com.txai.apipassenger.controller;
 
+import com.txai.common.dto.ResponseResult;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -8,6 +9,16 @@ public class PassengerController {
 
     @GetMapping("/test")
     public String test() {
-        return "{\"data\":\"test"+ System.currentTimeMillis()+"\"}";
+        return "{\"data\":\"test" + System.currentTimeMillis() + "\"}";
+    }
+
+    @GetMapping("/auth")
+    public ResponseResult auth() {
+        return ResponseResult.success();
+    }
+
+    @GetMapping("/noauth")
+    public ResponseResult noauth() {
+        return ResponseResult.success();
     }
 }
---
+++ api-passenger/src/main/java/com/txai/apipassenger/intercetpor/JwtInterceptor.java
@@ -0,0 +1,46 @@
+package com.txai.apipassenger.intercetpor;
+
+import com.alibaba.nacos.shaded.com.google.gson.JsonObject;
+import com.auth0.jwt.exceptions.AlgorithmMismatchException;
+import com.auth0.jwt.exceptions.SignatureVerificationException;
+import com.auth0.jwt.exceptions.TokenExpiredException;
+import com.txai.common.dto.ResponseResult;
+import com.txai.common.util.JwtUtils;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import org.apache.http.entity.ContentType;
+import org.json.JSONML;
+import org.json.JSONObject;
+import org.json.JSONStringer;
+import org.springframework.web.servlet.HandlerInterceptor;
+
+public class JwtInterceptor implements HandlerInterceptor {
+    @Override
+    public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws Exception {
+
+        String authorizetion = request.getHeader("Authorizetion");
+        String resultString = "";
+        boolean result = true;
+        try {
+            JwtUtils.parseToken(authorizetion);
+        } catch (SignatureVerificationException e) {
+            resultString = "token sign error";
+            result = false;
+        } catch (TokenExpiredException e) {
+            resultString = "token expired";
+            result = false;
+        } catch (AlgorithmMismatchException e) {
+            resultString = "token Algorithm mismatch";
+            result = false;
+        } catch (Exception e) {
+            resultString = "token invalid";
+            result = false;
+        }
+        if (!result) {
+            System.out.println("Error message : "+ resultString);
+            response.setContentType(ContentType.APPLICATION_JSON.getMimeType());
+            response.getWriter().write(JSONStringer.valueToString(new JSONObject(ResponseResult.fail().setMessage(resultString))));
+        }
+        return result;
+    }
+}
