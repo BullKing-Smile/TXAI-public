341c45331e4d484ba6db6fc105ac460e0393fe40
BullKing-Smile <bull.king.smile@gmail.com>
2025-06-20 15:33:26 +0800

    common independence

---
+++ txai-common/src/main/java/com/txai/common/request/VerificationCodeCheckDTO.java
@@ -0,0 +1,13 @@
+package com.txai.common.request;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class VerificationCodeCheckDTO {
+    private String passengerPhone;
+    private String verificationCode;
+}
